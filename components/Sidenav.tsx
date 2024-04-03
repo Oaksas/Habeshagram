60d7b61142154d5734da03534c1fede485cbb8d5
=oaksas <=>
2024-04-03 10:30:27 +0300

    Add components with themeprovider

--- components/Sidenav.tsx
+++ components/Sidenav.tsx
@@ -1,5 +1,26 @@
+import Logo from "./section/Logo";
+import MoreDropDown from "./section/MoreDropDown";
+import Navlinks from "./section/Navlinks";
+import Profile from "./section/Profile";
+
 function Sidenav() {
-  return <div>Sidenav</div>;
+  return (
+    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+      <div
+        className="border-t -ml-3 md:ml-0 bg-white dark:bg-neutral-950 h-16 justify-evenly 
+                  fixed z-50  flex-1 w-full md:relative md:h-full bottom-0 md:border-none flex flex-row
+                   md:justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2 p-2"
+      >
+        <Logo />
+        <Navlinks />
+        {/*If user is logged in*/}
+        <Profile />
+        <div className="hidden md:flex relative md:mt-auto flex-1 items-end w-full">
+          <MoreDropDown />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Sidenav;
